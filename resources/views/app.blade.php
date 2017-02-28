c3511990eaf75f3b835b7718e3b7b14c4e3050f6
Awake <alanka@contourgroupinc.com>
2017-02-28 09:05:40 +0530

    Added header and footer

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -21,41 +21,25 @@
 </head>
 <body>
 	<nav class="navbar navbar-default">
-		<div class="container-fluid">
-			<div class="navbar-header">
-				<!-- <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-					<span class="sr-only">Toggle Navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button> -->
-				<a class="navbar-brand" href="#">
-					<img src="{{ asset('/images/nimcare.png')}}">
-				</a>
-			</div>
-
-			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-				<!-- <ul class="nav navbar-nav">
-					<li><a href="{{ url('/') }}">Home</a></li>
-				</ul> -->
-
-				<ul class="nav navbar-nav navbar-right">
-					@if (Auth::guest())						
-					@else
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
-							<ul class="dropdown-menu" role="menu">
-								<li><a href="{{ url('/auth/logout') }}">Logout</a></li>
-							</ul>
-						</li>
-					@endif
-				</ul>
-			</div>
+		<div class="logo">
+			<a href="/"><img src="{{asset('/images/nimcare.png')}}"></a>		
+		</div>
+		<div class="brand">
+			<a href=""><img src="{{asset('/images/ecap-sol.png')}}" width="180"></a>		
 		</div>
 	</nav>
 
 	@yield('content')
 
+	<!-- Footer -->
+	<footer>
+		<div class="logo">
+			<a href="/"><img src="{{asset('/images/nimcare.png')}}"></a>		
+		</div>
+		<div class="copyright">
+			<span><i>Powered By</i> &nbsp;<a href="">www.ecapandsol.com</a></span>
+		</div>
+	</footer>
 	<!-- Scripts -->
 	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 	<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
